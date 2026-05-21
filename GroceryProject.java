fa62fef3547fe28203f3af06d2f9b7f5d2c365b7
Vicky Kumar <vickykumar14032006@gmail.com>
2026-05-21 10:47:51 +0530

    Update GroceryProject.java

--- GroceryProject.java
+++ GroceryProject.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 class Item {
-
-    private String name;
-    private double price;
-    private int qty;
+    String name;
+    double price;
+    int qty;
 
     Item(String name, double price, int qty) {
         this.name = name;
@@ -12,105 +11,66 @@ class Item {
         this.qty = qty;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public double getTotal() {
+    double total() {
         return price * qty;
     }
 }
 
-
 class Bill {
-
-    private Item[] items;
+    Item[] items;
 
     Bill(Item[] items) {
         this.items = items;
     }
 
-    public void generateReceipt() {
-
+    void showBill() {
         double total = 0;
 
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("\n====== Grocery Bill ======\n");
-        sb.append("----------------------------------\n");
-        sb.append("Item\tPrice\tQty\tTotal\n");
-        sb.append("----------------------------------\n");
+        System.out.println("\n---- BILL ----");
+        System.out.println("Name\tPrice\tQty\tTotal");
 
         for (Item i : items) {
-
-            double itemTotal = i.getTotal();
-
-            sb.append(i.getName())
-              .append("\t")
-              .append(i.getPrice())
-              .append("\t")
-              .append(i.getQty())
-              .append("\t")
-              .append(itemTotal)
-              .append("\n");
-
+            double itemTotal = i.total();
+            System.out.println(i.name + "\t" + i.price + "\t" + i.qty + "\t" + itemTotal);
             total += itemTotal;
         }
 
         double tax = total * 0.05;
-
         double grandTotal = total + tax;
 
-        sb.append("----------------------------------\n");
-        sb.append("Total: ").append(total).append("\n");
-        sb.append("Tax (5%): ").append(tax).append("\n");
-        sb.append("Grand Total: ").append(grandTotal).append("\n");
-
-        System.out.println(sb);
+        System.out.println("----------------");
+        System.out.println("Total: " + total);
+        System.out.println("Tax: " + tax);
+        System.out.println("Grand Total: " + grandTotal);
     }
 }
 
-
 public class GroceryProject {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of items: ");
+        System.out.print("How many items? ");
         int n = sc.nextInt();
 
-        Item[] arr = new Item[n];
+        Item[] items = new Item[n];
 
         for (int i = 0; i < n; i++) {
+            System.out.println("\nItem " + (i + 1));
 
-            sc.nextLine();
-
-            System.out.println("\nEnter details of Item " + (i + 1));
-
-            System.out.print("Item Name: ");
-            String name = sc.nextLine();
+            System.out.print("Name: ");
+            String name = sc.next();
 
             System.out.print("Price: ");
             double price = sc.nextDouble();
 
-            System.out.print("Quantity: ");
+            System.out.print("Qty: ");
             int qty = sc.nextInt();
 
-            arr[i] = new Item(name, price, qty);
+            items[i] = new Item(name, price, qty);
         }
 
-        Bill b = new Bill(arr);
-
-        b.generateReceipt();
+        Bill bill = new Bill(items);
+        bill.showBill();
 
         sc.close();
     }
